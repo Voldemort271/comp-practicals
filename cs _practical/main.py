a080d77438d96ef25b7cc80ae51ff3e911fa61ee
Voldemort <71368711+Voldemort271@users.noreply.github.com>
2022-12-31 18:14:13 +0530

    Delete cs _practical directory

--- cs _practical/main.py
+++
@@ -1,8 +0,0 @@
-# Wow, such empty
-
-a=[1,2,3,4]
-s=0
-for a[-1] in a:
-    print(a[-1])
-    s+=a[-1]
-print("sum=",s)
--- cs _practical/ques23.py
+++
@@ -1,5 +0,0 @@
-'''
-Ques 23
-
-Write a menu based program to add, delete and display the records of the hostel using list as stack data structure in python. Record of the hostel contains the fields : Hostel number, Total Students and Total Rooms. 
-'''
